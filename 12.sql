6707884a6b93abb2722454b7a9322e7b103c8985
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 18:18:51 +0530

    Add files via upload

---
+++ 12.sql
@@ -0,0 +1,16 @@
+-- Practice Question 
+-- Q12.Write a query to display only the songs performed by Neon Coast.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol");
+
+SET SQL_SAFE_UPDATES = 0;
+
+select *from songs where artist = "Neon Coast";

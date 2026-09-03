fe9d721f3db42cac37434b6cbbb1bb0a06996e9b
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 20:31:05 +0530

    Add files via upload

---
+++ 30.sql
@@ -0,0 +1,19 @@
+-- Practice Question 
+-- Q30.Write a query to find how many songs have the letter a anywhere in their title.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol"),
+("Who says?","Salena Gomez"),
+("Billie Jean","Michael Jackson "),
+("Dreams","Fleetwood Mac");
+
+SET SQL_SAFE_UPDATES = 0;
+	
+select count(*) as total_count from songs where title like "%a%";

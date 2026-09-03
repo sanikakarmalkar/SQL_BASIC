d7034c6b15e419bc35d34a788692b9283b922465
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 18:31:24 +0530

    Add files via upload

---
+++ 15.sql
@@ -0,0 +1,20 @@
+-- Practice Question 
+-- Q15.Write a query to display only the first 2 songs from the songs table.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol"),
+("Who says?","Salena Gomez"),
+("Billie Jean","Michael Jackson "),
+("Dreams","Fleetwood Mac");
+
+SET SQL_SAFE_UPDATES = 0;
+
+select *from songs  limit 2;
+

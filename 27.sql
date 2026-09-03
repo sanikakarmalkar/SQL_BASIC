08cea3c388acbf1cb9e2fd6686253261eb6d8b05
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 20:22:49 +0530

    Add files via upload

---
+++ 27.sql
@@ -0,0 +1,19 @@
+-- Practice Question 
+-- Q27.Write a query to find how many songs are performed by each artist whose name contains the letter o.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol"),
+("Who says?","Salena Gomez"),
+("Billie Jean","Michael Jackson "),
+("Dreams","Fleetwood Mac");
+
+SET SQL_SAFE_UPDATES = 0;
+
+select artist ,count(*) from songs where artist like "%o%" group by artist;

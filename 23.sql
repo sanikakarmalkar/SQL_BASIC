b7b071288ba2d27f8990c0b4efd2b0102e7bde95
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 20:04:14 +0530

    Add files via upload

---
+++ 23.sql
@@ -0,0 +1,19 @@
+-- Practice Question 
+-- Q23.Write a query to find the average song_id from the songs table.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol"),
+("Who says?","Salena Gomez"),
+("Billie Jean","Michael Jackson "),
+("Dreams","Fleetwood Mac");
+
+SET SQL_SAFE_UPDATES = 0;
+
+select avg(song_id) as average_song_id from songs;

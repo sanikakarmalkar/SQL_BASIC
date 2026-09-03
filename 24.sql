cda08a98384f144e28fc54ea986ef97637e805b1
Sanika Karmalkar <sanikakarmalkar0245@gmail.com>
2026-09-03 20:07:11 +0530

    Add files via upload

---
+++ 24.sql
@@ -0,0 +1,19 @@
+-- Practice Question 
+-- Q24.Write a query to find the total (sum) of all song_id values in the songs table.
+
+create database platform;
+use platform;
+create table songs(song_id bigint primary key auto_increment,title varchar(100),artist varchar(50) ) ;
+
+insert into songs(title,artist) values
+("Starlight Signal","Mira Sol"),
+("Ocean Static","Neon Coast"),
+("Velvet Rain","Neon Coast"),
+("Afterglow","Mira Sol"),
+("Who says?","Salena Gomez"),
+("Billie Jean","Michael Jackson "),
+("Dreams","Fleetwood Mac");
+
+SET SQL_SAFE_UPDATES = 0;
+
+select sum(song_id) as total_song_id from songs;
